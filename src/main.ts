f6502c151baf4edf6b3b745944b8d81cbe2364ac
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-01-18 13:33:12 +0000

    test: run

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import * as core from '@actions/core';
 import {Inputs} from './interfaces';
 import {getInputs} from './get-inputs';
 
-export async function run(): Promise<void> {
+export async function run(): Promise<number> {
   try {
     const inps: Inputs = getInputs();
 
     core.debug(`PublishBranch: ${inps.PublishBranch}`);
+
+    return 0;
   } catch (e) {
     core.setFailed(`Action failed with error ${e}`);
     return e;
