b500ce838db2923c6309008a8f091b731f6fa0e2
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-01-29 04:17:10 +0000

    fix: action failure status

--- src/main.ts
+++ src/main.ts
@@ -5,29 +5,24 @@ import {getInputs} from './get-inputs';
 import {setTokens} from './set-tokens';
 import * as git from './git-utils';
 
-export async function run(): Promise<number> {
-  try {
-    const inps: Inputs = getInputs();
+export async function run(): Promise<void> {
+  const inps: Inputs = getInputs();
 
-    await git.setConfig(inps);
+  await git.setConfig(inps);
 
-    const remoteURL = await setTokens(inps);
-    core.info(`[INFO] remoteURL: ${remoteURL}`); // TODO: remove
+  const remoteURL = await setTokens(inps);
+  core.info(`[INFO] remoteURL: ${remoteURL}`); // TODO: remove
 
-    await git.setRepo(inps, remoteURL);
+  await git.setRepo(inps, remoteURL);
 
-    await exec.exec('git', ['remote', 'rm', 'origin']);
-    await exec.exec('git', ['remote', 'add', 'origin', remoteURL]);
-    await exec.exec('git', ['add', '--all']);
+  await exec.exec('git', ['remote', 'rm', 'origin']);
+  await exec.exec('git', ['remote', 'add', 'origin', remoteURL]);
+  await exec.exec('git', ['add', '--all']);
 
-    await git.commit();
-    await git.push(inps.PublishBranch);
+  await git.commit();
+  await git.push(inps.PublishBranch);
 
-    core.info('[INFO] successfully deployed');
+  core.info('[INFO] successfully deployed');
 
-    return 0;
-  } catch (e) {
-    core.setFailed(`Action failed with error ${e}`);
-    return e;
-  }
+  return;
 }
