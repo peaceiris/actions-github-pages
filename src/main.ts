aa2a3e452ba27836a68abd46b3e6ff81d10d6224
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-02-04 23:07:02 +0900

    test 5

--- src/main.ts
+++ src/main.ts
@@ -6,23 +6,27 @@ import {setTokens} from './set-tokens';
 import * as git from './git-utils';
 
 export async function run(): Promise<void> {
-  const inps: Inputs = getInputs();
+  try {
+    const inps: Inputs = getInputs();
 
-  await git.setConfig(inps);
+    await git.setConfig(inps);
 
-  const remoteURL = await setTokens(inps);
-  core.info(`[INFO] remoteURL: ${remoteURL}`); // TODO: remove
+    const remoteURL = await setTokens(inps);
+    core.info(`[INFO] remoteURL: ${remoteURL}`); // TODO: remove
 
-  await git.setRepo(inps, remoteURL);
+    await git.setRepo(inps, remoteURL);
 
-  await exec.exec('git', ['remote', 'rm', 'origin']);
-  await exec.exec('git', ['remote', 'add', 'origin', remoteURL]);
-  await exec.exec('git', ['add', '--all']);
+    await exec.exec('git', ['remote', 'rm', 'origin']);
+    await exec.exec('git', ['remote', 'add', 'origin', remoteURL]);
+    await exec.exec('git', ['add', '--all']);
 
-  await git.commit();
-  await git.push(inps.PublishBranch);
+    await git.commit();
+    await git.push(inps.PublishBranch);
 
-  core.info('[INFO] successfully deployed');
+    core.info('[INFO] successfully deployed');
 
-  return;
+    return;
+  } catch (e) {
+    return e;
+  }
 }
