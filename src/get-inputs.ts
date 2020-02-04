87e9eea0ba1f87ce3535323b663e42fbfd9ddb8b
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-02-05 03:48:07 +0900

    revert

--- src/get-inputs.ts
+++ src/get-inputs.ts
@@ -25,7 +25,7 @@ function showInputs(inps: Inputs): void {
 
 export function getInputs(): Inputs {
   const inps: Inputs = {
-    DeployKey: core.getInput('deploy_key').trim(),
+    DeployKey: core.getInput('deploy_key'),
     GithubToken: core.getInput('github_token'),
     PersonalToken: core.getInput('personal_token'),
     PublishBranch: core.getInput('publish_branch'),
