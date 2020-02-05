1333ef6f810edd7fd42a56b786be33553dcbb45a
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-02-05 14:04:38 +0900

    fix: add new line, ssh-keyscan comment

--- src/set-tokens.ts
+++ src/set-tokens.ts
@@ -27,14 +27,15 @@ export async function setSSHKey(
   const knownHosts = path.join(sshDir, 'known_hosts');
   // ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts on Ubuntu
   const cmdSSHkeyscanOutput = `\
+# github.com:22 SSH-2.0-babeld-1f0633a6
 github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
 `;
-  fs.writeFileSync(knownHosts, cmdSSHkeyscanOutput);
+  fs.writeFileSync(knownHosts, cmdSSHkeyscanOutput + '\n');
   core.info(`[INFO] wrote ${knownHosts}`);
   await exec.exec('chmod', ['600', knownHosts]);
 
   const idRSA = path.join(sshDir, 'github');
-  fs.writeFileSync(idRSA, inps.DeployKey);
+  fs.writeFileSync(idRSA, inps.DeployKey + '\n');
   core.info(`[INFO] wrote ${idRSA}`);
   await exec.exec('chmod', ['600', idRSA]);
 
@@ -45,7 +46,7 @@ Host github
     IdentityFile ~/.ssh/github
     User git
 `;
-  fs.writeFileSync(sshConfigPath, sshConfigContent);
+  fs.writeFileSync(sshConfigPath, sshConfigContent + '\n');
   core.info(`[INFO] wrote ${sshConfigPath}`);
   await exec.exec('chmod', ['600', sshConfigPath]);
 
